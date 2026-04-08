4ce02522e085a22227a91d1b74a8ff27be9230bb
Hyeonmin Seon <hmsun.es@esgroup.net>
2026-04-08 17:55:18 +0900

    style: 코드 스타일 수정 - 세미콜론 제거 및 일관된 코드 스타일 적용

--- week3-2/src/todo-app/src/main.js
+++ week3-2/src/todo-app/src/main.js
@@ -1,15 +1,14 @@
-import PrimeVue from 'primevue/config';
-import Aura from '@primeuix/themes/aura';
+import PrimeVue from 'primevue/config'
+import Aura from '@primeuix/themes/aura'
 import App from './App.vue'
-import { createApp } from 'vue';
-import './style.css';
+import { createApp } from 'vue'
+import './style.css'
 import 'primeicons/primeicons.css'
 
-
-const app = createApp(App);
+const app = createApp(App)
 app.use(PrimeVue, {
   theme: {
-    preset: Aura
-  }
-});
-app.mount('#app');
+    preset: Aura,
+  },
+})
+app.mount('#app')
